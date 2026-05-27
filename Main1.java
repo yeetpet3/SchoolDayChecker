af7764803581b1086c98cf62f96627957d143b1e
Dylan <jvo78010@gmail.com>
2026-05-27 17:38:00 -0400

    Refactor push notification tags based on day number

--- Main1.java
+++ Main1.java
@@ -57,7 +57,7 @@ public static void sendPushNotification(String message, String day2) {
     String tags = null; 
       if(dayNum%2==0)
             {
-            tags = "running_shoe";
+            tags = "athletic_shoe";
             }
         else if(dayNum==1 || dayNum==5 || dayNum==9)
             {
